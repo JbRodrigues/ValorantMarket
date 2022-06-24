8309863dd6dd9e34b35890707b2d622b1449cf2b
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2022-06-24 17:10:55 -0300

    Added js scripts

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 var arrayReturn = {};
 var number = 0;
 var category = null;
-if (arrayReturn != " ") {
-    $.ajax({
-        type: 'GET',
-        url: "https://valorant-api.com/v1/weapons",
-        data: "",
-        success: function (data) {
-            arrayReturn = data.data;
-        },
-    });
-}
-
-$(document).on('keypress', function (e) {
-    if (event.key === 'b') {
-        if (category == null) {
-            $.each(arrayReturn, function () {
-                category = arrayReturn[number].category.split("::");
-                if (category[1] != "Melee") {
-                    $("#" + category[1]).append('<div class="' + category[1] + ' "> <img src="' + arrayReturn[number].shopData.newImage + '" alt=""> <div>500</div> <div>' + arrayReturn[number].displayName + '</div> </div>');
-                }
-                number++;
-            });
-        }
-        
+$(document).ready(function () {
+    if (arrayReturn != " ") {
+        $.ajax({
+            type: 'GET',
+            url: "https://valorant-api.com/v1/weapons",
+            data: "",
+            success: function (data) {
+                arrayReturn = data.data;
+                
+                $.each(arrayReturn, function () {
+                    category = arrayReturn[number].category.split("::");
+                    if (category[1] != "Melee") {
+                        console.log(category[1])
+                        $("#"+category[1]).append(arrayReturn[number].shopData.cost)
+                        console.log(arrayReturn[number])
+                        $("#" + category[1]).append('<div id="' + category[1] + '"><img src="' + arrayReturn[number].shopData.newImage + '" alt="" class="img-gun"></div> <div>' + arrayReturn[number].shopData.cost + '</div> <div>' + arrayReturn[number].displayName + '</div> </div>');
+                    }
+                    number++;
+                });
+                number = 0;
+            },
+        });
     }
 });
