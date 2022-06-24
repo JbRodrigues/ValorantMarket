c2c9419137c52ae0f8e480f5b81927897e81ccc5
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2022-06-24 20:16:12 -0300

    Script js running well

--- script.js
+++ script.js
@@ -9,14 +9,11 @@ $(document).ready(function () {
             data: "",
             success: function (data) {
                 arrayReturn = data.data;
-                
+
                 $.each(arrayReturn, function () {
                     category = arrayReturn[number].category.split("::");
                     if (category[1] != "Melee") {
-                        console.log(category[1])
-                        $("#"+category[1]).append(arrayReturn[number].shopData.cost)
-                        console.log(arrayReturn[number])
-                        $("#" + category[1]).append('<div id="' + category[1] + '"><img src="' + arrayReturn[number].shopData.newImage + '" alt="" class="img-gun"></div> <div>' + arrayReturn[number].shopData.cost + '</div> <div>' + arrayReturn[number].displayName + '</div> </div>');
+                        $("."+category[1]).append('<div id="title">'+category[1]+'</div> <div class="box" id="'+category[1]+' " data-arma="'+arrayReturn[number].displayName+'"><div> <img src="'+arrayReturn[number].shopData.newImage+'" alt="" class="img-guns"></div> <div>'+arrayReturn[number].shopData.cost+'</div> <div>'+arrayReturn[number].displayName+'</div> </div>');
                     }
                     number++;
                 });
@@ -25,3 +22,5 @@ $(document).ready(function () {
         });
     }
 });
+
+//append( "<p>Test</p>" );
